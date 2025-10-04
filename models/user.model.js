256ac8aa4786acc4a70c67744d406c8a249dc16b
Rakesh709 <kumar.rakesh1997@outlook.com>
2025-10-04 11:57:45 +0530

    table for user

--- models/user.model.js
+++ models/user.model.js
@@ -1,9 +1,18 @@
-import { integer, pgTable, varchar,uuid } from "drizzle-orm/pg-core";
+
+import { integer, pgTable, varchar,uuid,timestamp,text } from "drizzle-orm/pg-core";
 
 
 export const usersTable = pgTable("users", {
   id: uuid().primaryKey().defaultRandom(),
-  name: varchar({ length: 255 }).notNull(),
-  age: integer().notNull(),
+
+  firstname: varchar('first_name',{length:55}).notNull(),
+  lastname: varchar('last_name',{length:55}).notNull(),
+
   email: varchar({ length: 255 }).notNull().unique(),
+
+  password:text().notNull(),
+  salt: text().notNull(),
+
+  createdAt: timestamp('created_at').defaultNow().notNull(),
+  updatedAt: timestamp('updated_at').$onUpdate(()=>new Date()),
 });
