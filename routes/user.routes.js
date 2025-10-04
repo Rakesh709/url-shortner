49d5f19774a00705a251b6aca233e04b99579d17
Rakesh709 <kumar.rakesh1997@outlook.com>
2025-10-04 19:06:52 +0530

    routes refacter code

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,9 +1,12 @@
 import express from "express"
 import { db } from "../db/index.js"
 import { usersTable } from "../models/index.js"
-import { eq } from "drizzle-orm";
-import {randomBytes,createHmac} from "crypto"
+
+import {getUserByEmail} from "../services/user.service.js"
+
 import {signupPostRequestBodySchema} from "../validation/request.validation.js"
+
+import {hashPasswordWithSalt} from "../utils/hash.js"
 
 const router = express.Router();
 
@@ -16,13 +19,12 @@ router.post('/signup', async (req, res) => {
 
     const {firstname,lastname,email,password} = validationResult.data
 
-    const [existingUser] = await db.select().from(usersTable).where(eq(usersTable.email, email))
+    const existingUser = await getUserByEmail(email)
 
 
     if(existingUser) return res.status(400).json({error:`User with email ${email} already exists!`})
 
-    const salt = randomBytes(256).toString("hex")
-    const hashedPassword = createHmac('sha256',salt).update(password).digest('hex')
+    const {salt,password:hashedPassword} = hashPasswordWithSalt(password)
 
     const [user] = await db.insert(usersTable).values({
         email,
